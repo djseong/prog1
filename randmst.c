ba2bd35cedd124324858a73f3badfa6187042196
Jack Obeng-Marnu <JackMac@Jacks-MacBook-Pro-7.local>
2017-02-27 04:35:53 -0500

    almost done

--- randmst.c
+++ randmst.c
@@ -3,7 +3,6 @@
 #include <time.h>
 #include <math.h>
 #include "mst-heap.c"
-#include "mst.c" 
 
 // struct for vertices with up to 4 dimensions
 typedef struct vertex {
@@ -19,58 +18,6 @@ float random_float(void)
     return (double) rand() / ((double) RAND_MAX);
 }
 
-
-/*
-// finds Euclidean distance between two points with the same dimension
-float distance(vertex point_a, vertex point_b, int dimension)
-{
-    float sum = pow(point_a.x - point_b.x, 2);
-
-    if (dimension >= 2)
-    {
-        sum += pow(point_a.y - point_b.y, 2);
-    }
-    if (dimension >= 3)
-    {
-        sum += pow(point_a.z - point_b.z, 2);
-    }
-    if (dimension == 4)
-    {
-        sum += pow(point_a.w - point_b.w, 2);
-    }
-    return sqrt(sum);
-}
-*/
-
-// float distance(int dimension, float *list_a, float *list_b)
-// {
-//     float sum = 0;
-
-//     int i,j;
-//     for (i = 0; i < dimension; i++)
-//     {
-//         sum += pow(list_a[i] - list_b[i], 2);
-//     }
-    
-//     return sqrt(sum);
-// }
-
-/*
-// prints array
-void print_array(int size, float **arr)
-{
-    int i,j;
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            printf(" %f", arr[i][j]);
-        }
-        printf("\n");
-    }
-}
-*/
-
 // prints array
 void print_array(int height, int width, float **arr)
 {
@@ -85,118 +32,9 @@ void print_array(int height, int width, float **arr)
     }
 }
 
-/*
-// prints vertex lists
-void print_vertex_list(int size, vertex vertex_list[size], int dimension)
-{
-    int i;
-    for (i = 0; i < size; i++)
-    {
-        printf("(%f", vertex_list[i].x);
-        if (dimension >= 2)
-        {
-            printf(", %f", vertex_list[i].y);
-        }
-        if (dimension >= 3)
-        {
-            printf(", %f", vertex_list[i].z);
-        }
-        if (dimension == 4)
-        {
-            printf(", %f", vertex_list[i].w);
-        }
-        printf(")\n");
-    }
-    printf("\n");
-}
-*/
-
-/*
-float** generate_matrix (int size, float **array, int dimension)
-{
-    int i,j;
-    int counter = 1;
-    vertex vertex_list [size];
-
-    // initializes array to all 0s
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++) 
-        {
-            array[i][j] = 0.0;
-        }
-    }
-
-        // for dimension 0, the weight of each edge is a random var
-    if (dimension == 0)
-    {
-        // assigns a random float between 0 and 1 to upper triangle of array
-        for (i = 0; i < size; i++)
-        {
-            for (j = counter; j < size; j++) 
-            {
-                array[i][j] = random_float();
-            }
-            counter++;
-        }
-    }
-    // for all other dimensions, the weight of each edge is the Euclidean distance between two vertices
-    // first randomly choose each component of each vertex, then calculate the Euclidean distance between each pair of vertices
-    else
-    {
-        // assigns a random float between 0 and 1 for each component of each vertex
-        for (i = 0; i < size; i++)
-        {
-            vertex_list[i].x = random_float();
-
-            if (dimension >= 2)
-            {
-                vertex_list[i].y = random_float();
-            }
-            if (dimension >= 3)
-            {
-                vertex_list[i].z = random_float();
-            }
-            if (dimension == 4)
-            {
-                vertex_list[i].w = random_float();
-            }
-        }
-        
-        print_vertex_list(size, vertex_list, dimension);
-        // printf("\n");
-        
-        counter = 0;
-        // assigns top triangle of array to the corresponding distances between vertices
-        for(i = 0; i < size - 1; i++)
-        {
-            for(j = counter; j < size; j++)
-            {
-                array[i][j] = distance(vertex_list[i], vertex_list[j], dimension); 
-            }
-            counter++;
-        }
-    }
-    
-
-    //reflects upper triangle of array to lower triangle
-    counter = 0;
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < counter; j++) 
-        {
-            array[i][j] = array[j][i];
-        }
-        counter++;
-    }
-
-    return array;
-}
-*/
-
 float** generate_matrix (int height, int width, float **arr)
 {
-    int i,j;   
+    int i,j;
     for (i = 0; i < height; i++)
     {
         for (j = 0; j < width; j++)
@@ -213,13 +51,14 @@ float** generate_matrix (int height, int width, float **arr)
 int main( int argc, char *argv[] )
 {
     srand(time(NULL));
-
+    
     // sets commandline arguments to variables
-    int numpoints = atoi(argv[1]);
-    int trials = atoi(argv[2]);
-    int dimension = atoi(argv[3]); 
-    float total = 0.0; 
-
+    int flag = atoi(argv[1]);
+    int numpoints = atoi(argv[2]);
+    int trials = atoi(argv[3]);
+    int dimension = atoi(argv[4]);
+    float total = 0.0;
+    
     // for the case where dimension == 0, we will generate one list, but we will treat it differently
     // than we would treat a dimensoion 2 list
     int dim_num = dimension;
@@ -227,7 +66,7 @@ int main( int argc, char *argv[] )
     {
         dim_num = 1;
     }
-
+    
     float **array;
     array = malloc(dim_num * sizeof(*array));
     int i;
@@ -235,28 +74,66 @@ int main( int argc, char *argv[] )
     {
         array[i] = malloc(numpoints * sizeof(**array));
     }
-    
-    // array = generate_matrix(size, array, dimension);    
-    //print_array(size, array);
-    // printf("%n"); 
-    // printf("Result for mst:\n");
-    // prim(array, size);
-    clock_t start; 
-    clock_t diff; 
-    int duration = 0; 
-    for (i = 0; i < trials; i++) {
-        start = clock(); 
-        array = generate_matrix(dim_num, numpoints, array);
-        //print_array(dim_num, numpoints, array); 
-        printf("done generating\n"); 
-        total += primHeap(array, numpoints, dim_num);
-        diff = clock() - start; 
-        duration = diff/ CLOCKS_PER_SEC; 
-        printf("Time taken for trial %d: %d\n", i, duration);
-        //prim(array, size); 
+    int result = -2;
+    int free_num = dim_num;
+    FILE *fp = stdout;
+    if (flag == 1) {
+        result = 0;
+        fp = fopen("result.txt", "a");
+        if (fp == NULL) {
+            printf("error\n");
+            return -1;
+        }
     }
-
-    printf("Result for %d trials using mst heap: %f\n", trials, total/(float) trials); 
-
+    if (flag == 1) {
+        fprintf(fp, "%s %d %d %d %d\n", "./randmst", flag, numpoints, trials, dimension);
+    }
+    do {
+        if (result == 1)
+            result = 2;
+        total = 0.0;
+        clock_t start;
+        clock_t diff;
+        int duration = 0;
+        if (flag == 1)
+            free_num = result;
+        array = malloc(free_num * sizeof(*array));
+        if (flag == 1) {
+            fprintf(fp, "%d %s\n", free_num, "dimension");
+        }
+        for (i = 0; i < free_num; i++)
+        {
+            array[i] = malloc(numpoints * sizeof(**array));
+        }
+        for (i = 0; i < trials; i++) {
+            start = clock();
+            if (flag == 1) {
+                array = generate_matrix(result, numpoints, array);
+                total += primHeap(array, numpoints, result);
+            }
+            else {
+                array = generate_matrix(dim_num, numpoints, array);
+                total += primHeap(array, numpoints, dimension);
+            }
+            diff = clock() - start;
+            duration = diff/ CLOCKS_PER_SEC;
+            if (flag == 1) {
+                fprintf(fp, "%s %s %s %s %d %s %d\n",
+                        "Time", "taken", "for", "trial", i+1, ":", duration);
+            }
+        }
+        if (flag == 1) {
+            fprintf(fp, "%s %s %d %s %s %f\n", "Result", "for", trials, "trials", ":", total/(float)trials);
+        }
+        else {
+            fprintf(fp, "%f %d %d %d\n", total/(float)trials, numpoints, trials, dimension);
+        }
+        for (i = 0; i < free_num; i++)
+        {
+            free(array[i]);
+        }
+        free(array);
+        result++;
+    } while (result > 0 && result < 5);
     return 0;
 }
